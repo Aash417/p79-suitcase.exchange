7c2429f7b2bb0611399aae66b7c465e5a593a46a
aashish_k <aashishkathat00@gmail.com>
2025-06-06 20:33:26 +0530

    fixed vercel deployment issue with prisma workaround

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 import withBundleAnalyzer from '@next/bundle-analyzer';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const withAnalyzer = withBundleAnalyzer({
    enabled: process.env.ANALYZE === 'true'
 });
 
 const nextConfig = {
-   reactStrictMode: true
-   // Optional: add any Next.js config here
+   reactStrictMode: true,
+   webpack: (config, { isServer }) => {
+      if (isServer) {
+         config.plugins = [...config.plugins, new PrismaPlugin()];
+      }
+      return config;
+   }
 };
 
 export default withAnalyzer(nextConfig);
